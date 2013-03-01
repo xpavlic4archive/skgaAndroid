5273653ffdfce8d2830c35ade995fc0e69a3b75d
Radim <radim.pavlicek@gmail.com>
2013-03-01 21:49:37 +0100

    closes #3

--- src/com/laurinka/skga/app/EditActivity.java
+++ src/com/laurinka/skga/app/EditActivity.java
@@ -10,16 +10,19 @@ import android.os.Bundle;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.laurinka.skga.app.storage.StorageHelper;
+
 /**
  * Backs edit screen.
+ * 
  * @author radimpavlicek
- *
+ * 
  */
 public class EditActivity extends ListActivity {
 	private ArrayList<String> data;
-	
+
 	private ArrayAdapter<String> adapter;
 	private SharedPreferences sharedPreferences;
 
@@ -45,7 +48,7 @@ public class EditActivity extends ListActivity {
 		/** Defining the ArrayAdapter to set items to ListView */
 		adapter = new ArrayAdapter<String>(this,
 				R.layout.simple_list_item_multiple_choice, data);
-				// Assigned a custom layout located in layout #davidm
+		// Assigned a custom layout located in layout #davidm
 
 		// Assign adapter to ListView
 		setListAdapter(adapter);
@@ -63,21 +66,35 @@ public class EditActivity extends ListActivity {
 			data.add(name);
 
 		}
-		this.data =  data;
+		this.data = data;
 	}
-	
+
+	/**
+	 * Select all.
+	 * @param view
+	 */
+	public void invert(View view) {
+		ListView lv = getListView();
+		boolean check = lv.isItemChecked(0);
+		int size = getListAdapter().getCount();
+		for (int i = 0; i <= size; i++) {
+			lv.setItemChecked(i, !check);
+		}
+	}
+
 	public void delete(View view) {
-        SparseBooleanArray checkedItemPositions = getListView().getCheckedItemPositions();
-        int itemCount = getListView().getCount();
-        List<String> numbers = StorageHelper.getNumbers(sharedPreferences);
-        for(int i=itemCount-1; i >= 0; i--){
-            if(checkedItemPositions.get(i)){
-                adapter.remove(data.get(i));
-                getListView().setItemChecked(i, false);
-            	String toBeDeleted = numbers.get(i);
-            	StorageHelper.removeNumber(sharedPreferences, toBeDeleted);
-            }
-        }
-        adapter.notifyDataSetChanged();
+		SparseBooleanArray checkedItemPositions = getListView()
+				.getCheckedItemPositions();
+		int itemCount = getListView().getCount();
+		List<String> numbers = StorageHelper.getNumbers(sharedPreferences);
+		for (int i = itemCount - 1; i >= 0; i--) {
+			if (checkedItemPositions.get(i)) {
+				adapter.remove(data.get(i));
+				getListView().setItemChecked(i, false);
+				String toBeDeleted = numbers.get(i);
+				StorageHelper.removeNumber(sharedPreferences, toBeDeleted);
+			}
+		}
+		adapter.notifyDataSetChanged();
 	}
 }
