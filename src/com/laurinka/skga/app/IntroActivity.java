6453889e81f0ba17fb9b6dde843f60866838e5fe
David Mrnustik <david.mrnustik@hotmail.com>
2013-03-05 15:08:09 +0100

    added fonts for IntroActivity.java

--- src/com/laurinka/skga/app/IntroActivity.java
+++ src/com/laurinka/skga/app/IntroActivity.java
@@ -1,14 +1,19 @@
 package com.laurinka.skga.app;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 /**
  * Back intro screen.
  * @author radimpavlicek
  *
  */
+@SuppressLint("NewApi")
 public class IntroActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,15 @@ public class IntroActivity extends Activity {
 
 		/** Setting a custom layout for the list activity */
 		setContentView(R.layout.intro);
+		
+		/** Added tyFace for Intro buttons */
+		Button myButton=(Button)findViewById(R.id.records_button);
+		Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/helvneue63medextobl.ttf");
+		myButton.setTypeface(typeFace);
+		
+		/** Hide ActionBar in IntroActivity screen*/
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 	}
 	
     public void about(View view) {
