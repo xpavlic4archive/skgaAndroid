a5155c2a40a4781e240afc8746d2ecd50218b785
Radim <radim.pavlicek@gmail.com>
2013-03-06 22:04:53 +0100

    show club when click in main activity

--- src/com/laurinka/skga/app/AddByCgfNumberActivity.java
+++ src/com/laurinka/skga/app/AddByCgfNumberActivity.java
@@ -1,7 +1,6 @@
 package com.laurinka.skga.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
--- src/com/laurinka/skga/app/MainActivity.java
+++ src/com/laurinka/skga/app/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import android.app.ListActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -18,6 +19,8 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import com.laurinka.skga.app.rest.CgfService;
 import com.laurinka.skga.app.rest.Hcp;
 import com.laurinka.skga.app.rest.OnSKGAHcpResponse;
 import com.laurinka.skga.app.rest.SkgaService;
@@ -74,39 +77,41 @@ public class MainActivity extends ListActivity {
 	}
 
 	private ArrayList<HashMap<String, String>> findData() {
-		
+
 		ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
 		{
-		List<String> numbers = StorageHelper.getSkgaNumbers(sharedPreferences);
-		String[] numbersA = numbers.toArray(new String[0]);
-		for (int i = 0; i < numbers.size(); i++) {
-			HashMap<String, String> map = new HashMap<String, String>();
-			String value = numbersA[i];
-			map.put(Constants.NR, value);
-			String hcp = sharedPreferences.getString(Constants.SKGA_HCP_PREFIX
-					+ value, "");
-			map.put(Constants.HCP, hcp);
-			String name = sharedPreferences.getString(Constants.SKGA_NAME_PREFIX
-					+ value, "");
-			map.put(Constants.NAME, name);
-			data.add(map);
-		}
+			List<String> numbers = StorageHelper
+					.getSkgaNumbers(sharedPreferences);
+			String[] numbersA = numbers.toArray(new String[0]);
+			for (int i = 0; i < numbers.size(); i++) {
+				HashMap<String, String> map = new HashMap<String, String>();
+				String value = numbersA[i];
+				map.put(Constants.NR, value);
+				String hcp = sharedPreferences.getString(
+						Constants.SKGA_HCP_PREFIX + value, "");
+				map.put(Constants.HCP, hcp);
+				String name = sharedPreferences.getString(
+						Constants.SKGA_NAME_PREFIX + value, "");
+				map.put(Constants.NAME, name);
+				data.add(map);
+			}
 		}
 		{
-		List<String> numbers = StorageHelper.getCgfNumbers(sharedPreferences);
-		String[] numbersA = numbers.toArray(new String[0]);
-		for (int i = 0; i < numbers.size(); i++) {
-			HashMap<String, String> map = new HashMap<String, String>();
-			String value = numbersA[i];
-			map.put(Constants.NR, value);
-			String hcp = sharedPreferences.getString(Constants.CGF_HCP_PREFIX
-					+ value, "");
-			map.put(Constants.HCP, hcp);
-			String name = sharedPreferences.getString(Constants.CGF_NAME_PREFIX
-					+ value, "");
-			map.put(Constants.NAME, name);
-			data.add(map);
-		}
+			List<String> numbers = StorageHelper
+					.getCgfNumbers(sharedPreferences);
+			String[] numbersA = numbers.toArray(new String[0]);
+			for (int i = 0; i < numbers.size(); i++) {
+				HashMap<String, String> map = new HashMap<String, String>();
+				String value = numbersA[i];
+				map.put(Constants.NR, value);
+				String hcp = sharedPreferences.getString(
+						Constants.CGF_HCP_PREFIX + value, "");
+				map.put(Constants.HCP, hcp);
+				String name = sharedPreferences.getString(
+						Constants.CGF_NAME_PREFIX + value, "");
+				map.put(Constants.NAME, name);
+				data.add(map);
+			}
 		}
 		return data;
 	}
@@ -145,47 +150,87 @@ public class MainActivity extends ListActivity {
 		ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
 		root.addView(progressBar);
 
-        updateItemOnIndex(position);
+		updateItemOnIndex(position, ShowClub.YES);
 	}
 
 	public void updateAll() {
 		try {
 			int count = getListAdapter().getCount();
 			for (int i = 0; i < count; i++) {
-				updateItemOnIndex(i);
+				updateItemOnIndex(i, ShowClub.NO);
 			}
 		} catch (IndexOutOfBoundsException ignored) {
 			// TODO radim
 		}
 	}
 
-	private void updateItemOnIndex(int position) {
+	enum ShowClub {
+		YES, NO;
+	}
+
+	private void updateItemOnIndex(int position, final ShowClub show) {
 		@SuppressWarnings("unchecked")
 		Map<String, String> map = (Map<String, String>) getListAdapter()
 				.getItem(position);
 		final String message = map.get(Constants.NR);
-		new SkgaService().queryHcp(message, new OnSKGAHcpResponse() {
-			public void onResponse(Hcp response) {
-				Log.i(this.getClass().toString(), response.toString());
-				sharedPreferences
-						.edit()
-						//
-						.putString(Constants.SKGA_HCP_PREFIX + message,
-								response.getHcp()) //
-						.putString(Constants.SKGA_CLUB_PREFIX + message,
-								response.getClub()) //
-						.putString(Constants.SKGA_NAME_PREFIX + message,
-								response.getName()) //
-						.commit();
-				// callBack(message, response);
-			}
+		List<String> skgaNmbrs = StorageHelper
+				.getSkgaNumbers(sharedPreferences);
+		if (skgaNmbrs.contains(message)) {
+			new SkgaService().queryHcp(message, new OnSKGAHcpResponse() {
+				public void onResponse(Hcp response) {
+					Log.i(this.getClass().toString(), response.toString());
+					sharedPreferences
+							.edit()
+							//
+							.putString(Constants.SKGA_HCP_PREFIX + message,
+									response.getHcp()) //
+							.putString(Constants.SKGA_CLUB_PREFIX + message,
+									response.getClub()) //
+							.putString(Constants.SKGA_NAME_PREFIX + message,
+									response.getName()) //
+							.commit();
+					if (show.equals(ShowClub.YES))
+						callBack(message, response);
+				}
 
-			public void onError(Integer errorCode, String errorMessage) {
-				Log.w(this.getClass().toString(), errorCode + " "
-						+ errorMessage);
-			}
+				public void onError(Integer errorCode, String errorMessage) {
+					Log.w(this.getClass().toString(), errorCode + " "
+							+ errorMessage);
+				}
 
-		});
+			});
+		} else {
+
+			new CgfService().queryHcp(message, new OnSKGAHcpResponse() {
+
+				public void onResponse(Hcp response) {
+					Log.i(this.getClass().toString(), response.toString());
+					sharedPreferences
+							.edit()
+							//
+							.putString(Constants.CGF_HCP_PREFIX + message,
+									response.getHcp()) //
+							.putString(Constants.CGF_CLUB_PREFIX + message,
+									response.getClub()) //
+							.putString(Constants.CGF_NAME_PREFIX + message,
+									response.getName()) //
+							.commit();
+					if (show.equals(ShowClub.YES))
+						callBack(message, response);
+				}
+
+				public void onError(Integer errorCode, String errorMessage) {
+					Log.w(this.getClass().toString(), errorCode + " "
+							+ errorMessage);
+				}
+
+			});
+		}
+	}
+
+	public void callBack(String message, Hcp response) {
+		showList();
+		Toast.makeText(this, response.getClub(), Toast.LENGTH_LONG).show();
 	}
 
 	public void close(View view) {
