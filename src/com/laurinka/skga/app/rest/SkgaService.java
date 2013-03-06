1181b9e2c9c7d5ae54de407bc42c1b58f9f1fc78
David Mrnustik <david.mrnustik@hotmail.com>
2013-03-06 20:58:43 +0100

    change url

--- src/com/laurinka/skga/app/rest/SkgaService.java
+++ src/com/laurinka/skga/app/rest/SkgaService.java
@@ -7,8 +7,8 @@ package com.laurinka.skga.app.rest;
  */
 public class SkgaService extends RestClient {
     public SkgaService() {
-		//super( "http://skga-radim.rhcloud.com/rest");
-		super( "http://192.168.56.1/rest/"); /* server for local test */
+		super( "http://skga-radim.rhcloud.com/rest");
+		//super( "http://192.168.56.1/rest/"); /* server for local test */
     }
 
 	public void queryHcp(String member,
